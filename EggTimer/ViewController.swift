12e31d5694f1feec68a532fcad8ee580ea1a94d0
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2020-04-01 17:41:48 +0900

    [Implement] 버튼을 눌렀을 때 타이머 기능 추가

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var secondsRemaining = 60
+    
+    var timer : Timer?
+    
+    let eggTimes = ["Soft" :5, "Medium" : 7 , "Hard" : 12]
 
+    @IBOutlet weak var titleLabel: UILabel!
 
+    @IBAction func onPressed(_ sender: UIButton) {
+        
+        titleLabel.text = "Running"
+        
+        if timer == nil {
+            
+            secondsRemaining = eggTimes[sender.currentTitle!]!
+
+            timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (it : Timer) in
+                if self.secondsRemaining > 0 {
+                    print ("\(self.secondsRemaining) seconds")
+                    self.secondsRemaining -= 1
+                } else {
+                    self.secondsRemaining = 60
+                    self.titleLabel.text = "Done"
+                    it.invalidate()
+                    self.timer = nil
+                }
+            }
+        }
+    }
 }
